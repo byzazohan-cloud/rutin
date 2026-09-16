3f8c6f6fea616348cd601b002713c3634307a4af
byzazohan-cloud <byzazohan@gmail.com>
2026-09-16 19:46:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE='rutin-clean-v2-v43.1';
+const CACHE='rutin-clean-v2-v43.2.2';
 const FILES=[
   './',
   './index.html',
-  './styles.css?v=v43.1',
-  './app.js?v=v43.1',
-  './clean_v2_upgrade.js?v=v43.1',
-  './clean_v2_v13.js?v=v43.1',
-  './v18_upgrade.js?v=v43.1',
+  './styles.css?v=v43.2.2',
+  './app.js?v=v43.2.2',
+  './clean_v2_upgrade.js?v=v43.2.2',
+  './clean_v2_v13.js?v=v43.2.2',
+  './v18_upgrade.js?v=v43.2.2',
   './manifest.json',
   './icon.svg',
   './icon-180.png',
