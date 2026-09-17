098c1b70f97740aa33173be6abadbecd0c8a184c
byzazohan-cloud <byzazohan@gmail.com>
2026-09-17 23:04:21 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rutin-clean-v2-v43.13.3-calendar-summary';
+const CACHE='rutin-clean-v2-v43.14-design-v2';
 const CORE=['./','./index.html','./styles.css','./app.js','./v4313_search_full_edit.js','./v43132_data_delete.js','./manifest.json','./icon.svg','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
