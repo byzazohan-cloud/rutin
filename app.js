3787a7da49dec0a40bffeb8c132ca7a57d84e818
byzazohan-cloud <byzazohan@gmail.com>
2026-09-18 21:05:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1185,7 +1185,7 @@ if('serviceWorker' in navigator){
     location.reload();
   });
   window.addEventListener('load',()=>{
-    navigator.serviceWorker.register('./sw.js?v=43.16.4.3-compact',{updateViaCache:'none'}).then(reg=>{
+    navigator.serviceWorker.register('./sw.js?v=43.16.9.1-stable',{updateViaCache:'none'}).then(reg=>{
       const activateWaiting=()=>{
         if(reg.waiting) reg.waiting.postMessage({type:'SKIP_WAITING'});
       };
